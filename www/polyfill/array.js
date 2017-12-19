d435c0d4bf2a231c34c3c832684283269e328a60
Mads Jon Nielsen <mjn@cego.dk>
2017-12-19 19:01:54 +0100

    Added findIndex and Array.from to array.js polyfill file.

--- www/polyfill/array.js
+++ www/polyfill/array.js
@@ -18,3 +18,31 @@ if (!Array.prototype.find) {
         return null;
     };
 }
+
+if (!Array.prototype.findIndex) {
+    /**
+     * @template T
+     * @this Array<T>
+     * @param {function(T, number, !Array<T>) : boolean} predicate
+     * @return {number}
+     */
+    Array.prototype.findIndex = function(predicate) {
+        var length = this.length,
+            value;
+
+        for (var i = 0; i < length; i++) {
+            value = this[i];
+            if (predicate(value, i, this)) {
+                return i;
+            }
+        }
+        return -1;
+    };
+}
+
+
+if (!Array.from) {
+    Array.from = function() {
+
+    }
+}
