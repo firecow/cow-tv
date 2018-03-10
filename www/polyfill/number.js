2063ead710e442543593affb2d18db9d383b789f
Mads Jon Nielsen <mjn@cego.dk>
2018-03-10 19:26:23 +0100

    Documented clamp

--- www/polyfill/number.js
+++ www/polyfill/number.js
@@ -1,4 +1,10 @@
 if (!Number.prototype.clamp) {
+
+    /**
+     * @param {number} min
+     * @param {number} max
+     * @return {number}
+     */
     Number.prototype.clamp = function(min, max) {
         return Math.min(Math.max(this, min), max);
     };
